84d8b1ed4e14fbe4b57c41925916be31ab88d961
Cao Minh Thông <167119636+minhthong041@users.noreply.github.com>
2026-06-19 16:43:07 +0700

    Add shopping cart API (#11)

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("core.urls")),
     path("api/auth/", include("accounts.urls")),
+    path("api/cart/", include("cart.urls")),
     path("api/catalog/", include("catalog.urls")),
 ]
